4360291efa347c8985865cc4f7b34537c6d99072
multipassport <mashross@gmail.com>
2022-12-15 14:26:20 +0300

    Refactor code

--- main.py
+++ main.py
@@ -1,34 +1,42 @@
 import asyncio
 import datetime
 import logging
+from argparse import Namespace
 from asyncio import Queue
 from asyncio.streams import StreamReader
 
 import aiofiles
-import configargparse
+from configargparse import ArgumentParser
 
 import gui
 
 logger = logging.getLogger(__name__)
 
 
-def parse_cli_args():
-    parser = configargparse.ArgumentParser(default_config_files=['./reader_config.cfg'])
+def parse_cli_args() -> ArgumentParser:
+    parser = ArgumentParser(default_config_files=['./reader_config.cfg'])
     parser.add('--host', help='Chat host')
     parser.add('--port', help='Chat port')
     parser.add('--history', help='Where to save chat history')
     return parser
 
 
-async def connect_to_chat(chat_queue: Queue, log_queue: Queue, reader: StreamReader, history_location: str):
-    await read_messages_from_history(chat_queue, history_location)
+async def connect_to_chat(
+        chat_queue: Queue,
+        log_queue: Queue,
+        config: Namespace,
+):
+    await read_messages_from_history(chat_queue, config.history)
+    reader, writer = await asyncio.open_connection(
+        config.host, config.port,
+    )
     while True:
         message = await get_message(reader)
         chat_queue.put_nowait(message)
         log_queue.put_nowait(message)
 
 
-async def save_messages(queue: Queue, path):
+async def save_messages(queue: Queue, path: str):
     while True:
         async with aiofiles.open(path, mode='a', encoding='utf-8') as file:
             message = await queue.get()
@@ -41,7 +49,7 @@ async def read_messages_from_history(queue: Queue, path: str):
             queue.put_nowait(message)
 
 
-async def get_message(reader: StreamReader):
+async def get_message(reader: StreamReader) -> str:
     chat_message = await reader.readline()
     message_time = datetime.datetime.now().strftime('%d.%m.%y %H:%M')
     decoded_message = chat_message.decode()
@@ -65,17 +73,10 @@ async def main():
     logger.debug('Connecting to chat')
 
     logger.debug(config)
-    chat_host = config.host
-    chat_port = config.port
-    history_location = config.history
-
-    reader, writer = await asyncio.open_connection(
-        chat_host, chat_port,
-    )
 
     gui_task = gui.draw(messages_queue, sending_queue, status_updates_queue)
-    sending_messages_task = connect_to_chat(messages_queue, history_queue, reader, history_location)
-    saving_history_task = save_messages(history_queue, history_location)
+    sending_messages_task = connect_to_chat(messages_queue, history_queue, config)
+    saving_history_task = save_messages(history_queue, config.history)
 
     await asyncio.gather(gui_task, sending_messages_task, saving_history_task)
 
